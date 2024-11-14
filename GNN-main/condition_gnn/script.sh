dec84f2faf81bae1bdd092e1a968ecc2f454fa36
Diottoo <124545691+Diottoo@users.noreply.github.com>
2024-11-13 23:25:55 -0500

    Update script.sh

--- GNN-main/condition_gnn/script.sh
+++ GNN-main/condition_gnn/script.sh
@@ -1,28 +1,19 @@
+
+
 #!/bin/bash
-#SBATCH --job-name=array
-#SBATCH --output=experiments/logs/array_er_%A_%a.out
-#SBATCH --error=experiments/logs/array_er_%A_%a.err
-#SBATCH --array=1-1
 #SBATCH --time=1:00:00
-#SBATCH --partition=caslake
-#SBATCH --mem=10G
-#SBATCH --account=pi-cdonnat
-
-# Print the task id.
-echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
-echo "My SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
-
-# Add lines here to run your computations
-job_id=$SLURM_ARRAY_JOB_ID
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=16G
 
 
-result_file="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
-echo "result file is ${result_file}"
-cd $SCRATCH/$USER/GNN/condition
+
+cd GNN-main/condition
 
 module load gsl
 module load gcc
 module load python/anaconda-2021.05
-python3 main.py  --namefile $result_file --seed $SLURM_ARRAY_TASK_ID --dataset $1 
+python3 main.py 
 \
 
